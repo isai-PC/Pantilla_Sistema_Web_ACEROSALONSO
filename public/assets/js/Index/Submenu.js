1710b42807777a52f80c13e68d60b4db8c186297
Cristopher Osorio Morales <20241042@uthh.edu.mx>
2026-03-08 15:14:43 -0600

    Se agrego API para el submenu de Productos y ahora la navegacion esta actualizada con un scroll

---
+++ public/assets/js/Index/Submenu.js
@@ -0,0 +1,32 @@
+(function() {
+    // Ahora API_URL solo existe dentro de esta "burbuja"
+    const API_URL = "https://repositorio-para-vercel-tawny.vercel.app/api/productos";
+    const menuLi = document.getElementById("SumenuProductos");
+
+    if (!menuLi) return;
+
+    const cargarMenu = async () => {
+        try {
+            const response = await fetch(API_URL);
+            const result = await response.json();
+            const productos = result.data || result;
+            const listaUl = menuLi.querySelector("ul");
+
+            if (!listaUl) return;
+            listaUl.innerHTML = "";
+
+            productos.forEach(producto => {
+                const id = producto.id_producto || producto.id;
+                listaUl.innerHTML += `
+                    <li class="w-full block">
+                        <a href="../VistaDetalleProducto/VistaVieew.html?id=${id}" 
+                        class="block px-5 py-3 border-b border-white/10 text-white hover:bg-orange-400 transition-all">
+                        ${producto.nombre_producto}
+                        </a>
+                    </li>`;
+            });
+        } catch (e) { console.error(e); }
+    };
+
+    document.addEventListener("DOMContentLoaded", cargarMenu);
+})(); // Los paréntesis finales ejecutan la función
