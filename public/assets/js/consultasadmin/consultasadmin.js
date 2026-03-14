037572dee522068f2bf836cdc0a1a795adfed55b
JuanOlvera06 <20240982@uthh.edu.mx>
2026-03-14 11:32:04 -0600

    arreglo

--- public/assets/js/consultasadmin/consultasadmin.js
+++ public/assets/js/consultasadmin/consultasadmin.js
@@ -2,7 +2,10 @@ if(!localStorage.getItem("token")){
     window.location.href = "../../../index.html";
 }
 const nombre = localStorage.getItem("nombre");
+const token = localStorage.getItem("token");
 
 if(nombre){
     document.getElementById("nombre").textContent = "Usuario: " + nombre;
 }
+
+
