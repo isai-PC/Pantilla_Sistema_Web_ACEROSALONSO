a147c562355b80a0107406cd883d12038e73cc73
Isaí Pascual Cruz <20241018@uthh.edu.mx>
2026-03-19 17:41:28 -0600

    correccion de https

--- public/assets/js/login/login.js
+++ public/assets/js/login/login.js
@@ -20,7 +20,7 @@ if (token) {
 
 
 /* const urlApi = "https://repositorio-para-vercel-tawny.vercel.app/api/grupos/login"; */
-const urlApi = "https://https://apis-propias-a-vercel.vercel.app/api/grupos/login";
+const urlApi = "https://apis-propias-a-vercel.vercel.app/api/grupos/login";
 
 const verficarSesion = () => {
     // Usamos fetch para hacer la petición HTTP
